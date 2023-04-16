03aebe8526e3d72157f15ca72bcb820116e16e0f
mustafa3li3li <130888555+mustafa3li3li@users.noreply.github.com>
2023-04-16 19:36:40 +0000

    ddd, Apr 4/16/2023, YYYY, 7:36 A +0

--- pyramid.c
+++ pyramid.c
@@ -2,17 +2,13 @@
 
 int main()
 {
-    int rows = 4; // Number of rows in the pattern
+int L;
 
-    for(int i=0; i<rows; i++) {
-        for(int j=0; j<rows; j++) {
-            if(j >= rows-i-1) {
-                printf("#");
-            }
-            else {
-                printf(" ");
-            }
-        }
-        printf("\n");
-    }
+printf("Enter the base length of the pryamid : ")
+scanf("%d",&L);
+
+for(int i=1;i<=L;i+2)
+{
+    printf
+}
 }
