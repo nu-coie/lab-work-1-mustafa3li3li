632a9ebb4e7b31933814079125dad84b983bbe4b
mustafa3li3li <130888555+mustafa3li3li@users.noreply.github.com>
2023-04-16 18:41:10 +0000

    ddd, Apr 4/16/2023, YYYY, 6:41 A +0

--- calculator.c
+++ calculator.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
-#include <cs50.h>
 
 
 int main(void)
 {
 // Accept Input From user (char), where (a) stands for "Addition", (m) for "Multiplications," and (d) for "Division,"
-  
+char in;
 
-  
+printf("Enter a letter : \n a- Additon \n m- Multiplication \n d-Divison");
+scanf(in);
+
+if(in==a)
+{
+    
+}
+
+
 
 }
