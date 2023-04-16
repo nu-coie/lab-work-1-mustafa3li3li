2f625ddbd59460a600924b2675137c6a0bb25484
mustafa3li3li <130888555+mustafa3li3li@users.noreply.github.com>
2023-04-16 19:54:51 +0000

    ddd, Apr 4/16/2023, YYYY, 7:54 A +0

--- pyramid.c
+++ pyramid.c
@@ -7,9 +7,9 @@ int L;
 printf("Enter the base length of the pryamid : ");
 scanf("%d", &L);
 
-for(int i=1;i<=L;i+2)
+for(int i=1;i<=L;i+=52)
   {
-    for(int j=0;j<=i;j+1)
+    for(int j=0;j<=i;j+=1)
         printf("#");
 
    printf("\n");
